21c2c0c7dff69137b581efb8ef7a3080dad53709
张柯 <413365742@qq.com>
2022-11-08 17:33:06 +0800

    update style

--- docs/components/card.vue
+++ docs/components/card.vue
@@ -21,8 +21,7 @@ const { topic, title, detail } = props.info
   align-items: flex-start;
   width: 100%;
   height: 100%;
-  background-color: aliceblue;
-  border: 1px solid var(--vp-c-bg-soft);
+  border: 2px solid var(--vp-c-bg-mute);
   border-radius: 10px;
   margin: 10px;
   padding: 24px;
